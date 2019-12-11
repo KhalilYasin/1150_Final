ebc4e25985e8e3f3da83150d1338488f78d768f4
Khalil <fr1832da@go.minnstate.edu>
2019-12-11 17:50:31 -0600

    adding last comments

--- 1150_Final.py
+++ 1150_Final.py
@@ -1,18 +1,19 @@
 """
 This is my final project. I chose the code that generates a recipe book from taco recipes including a random taco picture
 """
-import requests
-import sys
-from PIL import Image, ImageFont, ImageDraw
-from docx import Document
+import requests # import request library
+import sys # import sys library
+from PIL import Image, ImageFont, ImageDraw # import libraries for font and image
+from docx import Document # to create word document
 from docx.shared import Inches
-from pyunsplash import PyUnsplash
+from pyunsplash import PyUnsplash # import from Unsplash website
 
 # api for unsplash
 API_KEY = "6fe959d9f7971906d0cb9fa31671d876772c1b841405f91b914f2b55bd838257"
 # taco api url
 URL = "https://taco-1150.herokuapp.com/random/?full_taco=true"
 FONT_FILE = "DejaVuSans.ttf"
+
 
 # this function is to search for image
 def search_image(tag):
@@ -47,17 +48,17 @@ def resize_image():
     ratio = height / width
     # set image to max 800 in width and height avoiding all logical errors
     if (width > height):
-        width = 800
-        height = int(ratio * 800)
+        width = 800 # set width size to 800
+        height = int(ratio * 800) # multiply height ratio by 800
     else:
-        height = 800
-        width = int(800 / ratio)
+        height = 800 # set height to 800
+        width = int(800 / ratio) # multiply width ratio by 800
     im = im.resize((width, height), Image.ANTIALIAS)
-    # adding a font to the taco image
-    font = ImageFont.truetype(FONT_FILE, 40)
-    draw = ImageDraw.Draw(im)
-    draw.text((0, 0), "Random Taco Cookbook", (255, 255, 255), font=font)
-    im.save('taco_small.jpg', format='JPEG')
+    # adding font to taco image
+    font = ImageFont.truetype(FONT_FILE, 40) # setting font size
+    draw = ImageDraw.Draw(im) # drawing function
+    draw.text((0, 0), "Random Taco Cookbook", (255, 255, 255), font=font) # choosing pixels to place the font
+    im.save('taco_small.jpg', format='JPEG') # saving new modified image to taco small
 
 
 def make_request():
@@ -65,12 +66,13 @@ def make_request():
     this function returns data from taco api
     '''
     try:
-        data = requests.get(URL, verify=False)
+        data = requests.get(URL, verify=False) # returning data from taco API while verifying
         data = data.json()
     except:
-        print("Error requesting taco API")
+        print("Error requesting taco API") # statement prints out in case of error while requesting from API
         sys.exit(1)
     return data
+
 
 # used a function to create & customize document
 def create_document(data, image_name, photo_info):
@@ -79,37 +81,39 @@ def create_document(data, image_name, photo_info):
     document.add_heading("Random Taco Cookbook", 0)
     # adding a picture
     document.add_picture(image_name, width=Inches(5.5))
-    # adding and naming heading
+    # adding and naming heading for credits
     document.add_heading('Credits')
     # adding lines under credits
-    document.add_paragraph('Taco image: ' + photo_info, style='List Bullet')
-    document.add_paragraph('Tacos from: ' + URL, style='List Bullet')
-    document.add_paragraph('Code by: Khalil', style='List Bullet')
+    document.add_paragraph('Taco image: ' + photo_info, style='List Bullet') # adds unsplash link, styles it with bullet points
+    document.add_paragraph('Tacos from: ' + URL, style='List Bullet') # adds taco recipes API, styles it with bullet points
+    document.add_paragraph('Code by: Khalil', style='List Bullet') # adds my name, styles it with bullet points
 
-# this loops through the recipes in URL (taco recipes) and creates a document by listing them
-    for recipe in data:
-        document.add_page_break()
-        names = ', '.join(recipe[name]['name'] for name in recipe)
+    # this loops through the recipes in URL (taco recipes) and creates a document by listing them
+    for recipe in data: # loops each recipe in taco recipes
+        document.add_page_break() # adds a page break
+        names = ', '.join(recipe[name]['name'] for name in recipe) # creates a headline for each recipe
         document.add_heading(names, 0)
-        for rec in recipe:
-            document.add_heading(recipe[rec]['name'], level=1)
-            document.add_paragraph(recipe[rec]['recipe'])
-# after looping save the data in a word document
+
+        for rec in recipe:  # this loops through recipes list created from loop above, creates a list
+            document.add_heading(recipe[rec]['name'], level=1) # creates a heading for recipe list
+            document.add_paragraph(recipe[rec]['recipe']) # adds list of recipe
+    # after looping, save the data in a word document
     document.save("recipes.docx")
+
 
 """
 p = search_image('taco') will search in unsplash for one image that has 'taco' as tag. 
 resize_image() will resize that image (p), and that loop will store 5 recipes taken from that Taco API.
 create_document will use the resized image and the five recipes to create the book of recipes
 """
-def main():
-    p = search_image('taco')
-    resize_image()
-    recipes = []
-    for i in range(5):
-        recipes.append(make_request())
-    create_document(recipes, 'taco_small.jpg', p)
 
+def main():
+    p = search_image('taco') # search for image with taco in tag
+    resize_image() # use this function to resize the image
+    recipes = [] # create an empty list
+    for i in range(5): # create a sequence of recipes and print them in that sequence = 5
+        recipes.append(make_request()) # appending recipes
+    create_document(recipes, 'taco_small.jpg', p) # create the book of recipes
 
 if __name__ == '__main__':
     main()
@@ -117,8 +121,3 @@ if __name__ == '__main__':
 """
 The End...
 """
-
-
-
-
-
